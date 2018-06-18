d5c7b1df235d0105b20450bead6f2d038df40342
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-18 12:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  Object.assign({}, driver, {[key]: value})
+  newDriver = {...driver};
+  newDriver[key] = value;
+  return newDriver;
 }
