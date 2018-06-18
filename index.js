e640ebee92270817e17ad2a3a1eae37e4e4eab63
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-18 12:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
 }
+
+function deleteFromDriverByKey(driver, key) {
+  newDriver = {...driver};
+  delete newDriver.key;
+  return newDriver;
+}
